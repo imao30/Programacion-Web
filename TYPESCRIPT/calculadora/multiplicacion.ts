933f6c10473f81d6348eb9268aeafdc96194920a
imao30 <imanolelenes@gmail.com>
2025-11-05 09:26:37 -0700

    listo

---
+++ TYPESCRIPT/calculadora/multiplicacion.ts
@@ -0,0 +1,4 @@
+const multiplicar = (a, b, imprimir) => {
+  console.log(imprimir, a * b);
+};
+multiplicar(2, 4, "El resultado es");
---
+++ TYPESCRIPT/js/javascript.js
@@ -0,0 +1,11 @@
+/*TypeSript -> JavaScript
+  Conversion de lenguajes (Traspilador)
+  Transpilador : codigo de un lenguaje a otro*/
+
+let mensaje = "Hola javascript";
+console.log(mensaje);
+console.log(typeof mensaje);
+
+mensaje = 2;
+console.log(mensaje);
+console.log(typeof mensaje);
