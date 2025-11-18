944f76a1a5ca4e6b82fd00d4ee911c2c66900c16
imao30 <imanolelenes@gmail.com>
2025-11-18 12:58:24 -0700

    cambios del profe

---
+++ backend/src/schema/personal.Schema.ts
@@ -0,0 +1,9 @@
+import {z} from "zod";
+
+//validaciones con zod - contruir un schema
+export const personalSchema = z.object({
+    nombre: z.string().min(2,'Minimo 2 caracteres').max(200,'Maximo 200 caracteres'),
+    direccion: z.string().min(2).max(300),
+    telefono: z.string().min(10).max(15),
+    estatus: z.number('Solo numeros').int().positive('solo numeros positivos').min(1).max(2),
+})
--- backend/src/services/personalServices.ts
+++ backend/src/services/personalServices.ts
@@ -1,56 +1,79 @@
-import mysql from 'mysql2/promise';
-import { type Personal,type PersonalNuevo } from '../typesPersonal.js';
+import mysql from "mysql2/promise";
+import { type Personal, type PersonalNuevo } from "../typesPersonal.js";
+import { personalSchema } from "../schema/personal.Schema.js";
 
 const conexion = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
+  host: "localhost",
+  user: "root",
   password: "",
-  database: 'pw12001200',
+  database: "pw12001200",
 });
 
-export const obtienePersonal = async () =>{
-    try{
-        // const [results,fields]
-        const [results] = await conexion.query("SELECT * FROM personal");
-        return results;
-    }catch(err){
-        return {error: "No se puede obtener la lista de personal"};
-    }
-}
+export const obtienePersonal = async () => {
+  try {
+    // const [results,fields]
+    const [results] = await conexion.query("SELECT * FROM personal");
+    return results;
+  } catch (err) {
+    return { error: "No se puede obtener la lista de personal" };
+  }
+};
 
-export const encuentraPersonal = async (id:number) =>{
-    try{
-        // const [results,fields]
-        const [results] = await conexion.query("SELECT * FROM personal WHERE id = ? LIMIT 1",[id]);
-        return results;
-    }catch(err){
-        return {error: "No se puede obtener la lista de personal"};
-    }
-}
+export const encuentraPersonal = async (id: number) => {
+  try {
+    // const [results,fields]
+    const [results] = await conexion.query(
+      "SELECT * FROM personal WHERE id = ? LIMIT 1",
+      [id]
+    );
+    return results;
+  } catch (err) {
+    return { error: "No se puede obtener la lista de personal" };
+  }
+};
 
-export const agregarPersonal = async (nuevo:PersonalNuevo) =>{
-    try {
-        const [results] = await conexion.query("INSERT INTO personal(nombre,direccion,telefono,estatus) VALUES(?,?,?,?)",[nuevo.nombre,nuevo.direccion,nuevo.telefono,nuevo.estatus]);
-        return results;
-    } catch (error) {
-        return {error: "No se puede agregar al personal."}
+export const agregarPersonal = async (nuevo: PersonalNuevo) => {
+  try {
+    const validacion = personalSchema.safeParse(nuevo);
+    if (!validacion.success) {
+      return { error: "Error de validacion:", detalles: validacion.error };
     }
-}
+    const [results] = await conexion.query(
+      "INSERT INTO personal(nombre,direccion,telefono,estatus) VALUES(?,?,?,?)",
+      [nuevo.nombre, nuevo.direccion, nuevo.telefono, nuevo.estatus]
+    );
+    return results;
+  } catch (error) {
+    return { error: "No se puede agregar al personal." };
+  }
+};
 
-export const modificarPersonal = async (modificado:Personal) =>{
-    try {
-        const [results] = await conexion.query("UPDATE personal SET nombre=?,direccion=?,telefono=?,estatus=? WHERE id = ?",[modificado.nombre,modificado.direccion,modificado.telefono,modificado.estatus,modificado.id]);
-        return results;
-    } catch (error) {
-        return {error: "No se puede agregar al personal."}
-    }
-}
+export const modificarPersonal = async (modificado: Personal) => {
+  try {
+    const [results] = await conexion.query(
+      "UPDATE personal SET nombre=?,direccion=?,telefono=?,estatus=? WHERE id = ?",
+      [
+        modificado.nombre,
+        modificado.direccion,
+        modificado.telefono,
+        modificado.estatus,
+        modificado.id,
+      ]
+    );
+    return results;
+  } catch (error) {
+    return { error: "No se puede agregar al personal." };
+  }
+};
 
-export const borrarPersonal = async (id:number) =>{
-    try {
-        const [results] = await conexion.query("DELETE FROM personal WHERE id = ?",[id]);
-        return results;
-    } catch (error) {
-        return {error: "No se puede borrar el personal"};
-    }
-}
+export const borrarPersonal = async (id: number) => {
+  try {
+    const [results] = await conexion.query(
+      "DELETE FROM personal WHERE id = ?",
+      [id]
+    );
+    return results;
+  } catch (error) {
+    return { error: "No se puede borrar el personal" };
+  }
+};
