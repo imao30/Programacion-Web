541d0f8a733bc527151037a6746e698367fd5e57
imao30 <imanolelenes@gmail.com>
2025-11-20 12:56:35 -0700

    primera implementacion de vue

--- backend/build/schema/personal.Schema.js
+++ backend/build/schema/personal.Schema.js
@@ -1,8 +1,10 @@
 import { z } from "zod";
+const telefonoRegEx = new RegExp(/^[+]?(\d{1,2})?[\s.-]?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]$/);
 //validaciones con zod - contruir un schema
 export const personalSchema = z.object({
     nombre: z.string().min(2, 'Minimo 2 caracteres').max(200, 'Maximo 200 caracteres'),
     direccion: z.string().min(2).max(300),
-    telefono: z.string().min(10).max(15),
+    //telefono: z.string().min(10).max(15),
+    telefono: z.string().regex(telefonoRegEx, 'Numero de telefono invalido'),
     estatus: z.number('Solo numeros').int().positive('solo numeros positivos').min(1).max(2),
 });
---
+++ frontend/cliente_tienda/src/modulos/principal/vistas/MenuVue.vue
@@ -0,0 +1,9 @@
+<template>
+  <div></div>
+</template>
+
+<script setup lang="ts"></script>
+
+//scoped es para que los estilos solo afecten a este componente
+
+<style scoped></style>
